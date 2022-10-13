15f9a0044ad4a1a021b8ae97fb90ec0f3fc2b808
Wongsathorn632115035 <wongsathorn_w@cmu.ac.th>
2022-10-14 00:16:04 +0700

    6.8

--- src/main/java/se331/rest/util/LabMapper.java
+++ src/main/java/se331/rest/util/LabMapper.java
@@ -1,15 +1,14 @@
 package se331.rest.util;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import se331.rest.entity.Event;
-import se331.rest.entity.EventDTO;
-import se331.rest.entity.Organizer;
-import se331.rest.entity.OrganizerDTO;
+import se331.rest.entity.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper
+@Mapper(imports = Collectors.class)
 public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
 
@@ -21,5 +20,7 @@ public interface LabMapper {
 
     List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
 
-//    OrganizerDTO getOrganizerAuthDTO(Organizer organizer);
+    @Mapping(target = "authorities",
+            expression = "java(organizer.getUser().getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
+    OrganizerAuthDTO getOrganizerAuthDTO(Organizer organizer);
 }
