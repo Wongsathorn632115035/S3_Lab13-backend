9304342aadd04af0affa0df82be0405ee31da310
markwst <Wongsathorn_W@gmail.com>
2022-10-17 10:17:23 +0700

    deploy

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -19,7 +19,7 @@ public class Lab08Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://52.23.234.62:4000")
+                        .allowedOrigins("http://localhost:3000","http://54.83.89.173:4000")
                         .exposedHeaders("x-total-count");
 
             }
